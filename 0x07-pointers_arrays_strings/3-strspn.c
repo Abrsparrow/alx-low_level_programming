611cb1cce975b3e683ff1e1e0558f8270842ebc3
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-03 19:04:41 -0700

    a funtion that that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,11 +7,10 @@
 * _strspn - returns the count of characters of accept that exist in s
 *
 * @s: string where accept bytes exist or not
-* @accept: bytes of data to look up in s
+* @accept: bytes of data to look  in s
 *
 * Return: length of prefix substring
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	return (strspn(s, accept));
