0cd87db124a52283d63097c95ee811ec46822825
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-07 15:32:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * print_number - prints given number
 *
@@ -20,4 +21,41 @@ void print_number(int n)
 		print_number(n / 10);
 	}
 	putchar(n % 10 + '0');
+}
+/**
+* main - Entree point, prints out all arguments of program
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0, denoting success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 3)
+	{
+		putchar('E');
+		putchar('r');
+		putchar('r');
+		putchar('o');
+		putchar('r');
+		putchar('\n');
+
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	print_number(result);
+	putchar('\n');
+
+	return (0);
 }
