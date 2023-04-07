784a4f003109de965e4c2bc8986349b1e95629f6
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-07 15:15:36 -0700

    README, main.h ,0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
