00b9eb5aa9a2810726a3cb18bbe83c13d573b32f
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-12 10:41:25 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
- */
+* malloc_checked - allocates memory using malloc
+*
+* @b: variable to which memory will be allocated to
+*
+* Return: address of the memory created for b
+*/
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *address;
 
-	ptr = malloc(b);
+	address = malloc(b);
 
-	if (ptr == NULL)
+	if (address == NULL)
+	{
 		exit(98);
+	}
 
-	return (ptr);
+	return (address);
 }
