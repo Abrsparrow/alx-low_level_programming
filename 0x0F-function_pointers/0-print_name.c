f60311ebb45840e1a991e7e338beaf5bbe140ca8
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-19 08:25:54 -0700

    betty fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+0-print_name.c
+
 #include "function_pointers.h"
 #include <stdio.h>
 /**
@@ -9,9 +11,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-
 		return;
-	
 
 	f(name);
 }
