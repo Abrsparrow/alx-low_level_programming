666ec205014ffae25f7fc831dc194dafeb5f4d26
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-19 19:53:51 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,86 @@
-i#include "3-calc.h"
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
+ *  op_add - adds given parameters
  *
- * Return: The sum of a and b.
+ *	@a: integer to be added
+ *	@b: integer to be added
+ *
+ *  Return: result of addition of the two parameters
+ *
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
+ *  op_sub - subtracts given parameters
  *
- * Return: The difference of a and b.
+ *	@a: integer to be subtracted
+ *	@b: integer to be subtracted
+ *
+ *  Return: result of subtraction of the two parameters
+ *
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
+ *  op_mul - multiplies given parameters
  *
- * Return: The product of a and b.
+ *	@a: integer to be multiplied
+ *	@b: integer to be multiplied
+ *
+ *  Return: result of multiplication of the two parameters
+ *
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ *  op_div - divides given parameters
  *
- * Return: The quotient of a and b.
+ *	@a: integer to be divided
+ *	@b: integer to be divided
+ *
+ *  Return: result of division of the two parameters
+ *
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
+
 /**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ *  op_mod - performs modulo on given parameters
  *
- * Return: The remainder of the division of a by b.
+ *	@a: integer to be divided and checked for modulo
+ *	@b: integer to be divided and checked for modulo
+ *
+ *  Return: result of modulo of the two parameters
+ *
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
