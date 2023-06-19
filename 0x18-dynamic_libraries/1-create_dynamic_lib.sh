33a836ee9404f6097e94fefc178fe8eaf09b6076
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-06-19 09:26:53 -0700

    samll fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic -c *.c 
+gcc -fPIC -Wall -Werror -Wextra -pedantic -c *.c 
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
